56ebb88cde3209c44f74dd45ac46deed79209f3c
benneely <nigelneely@gmail.com>
2018-09-28 11:03:41 -0400

    changing loss function to sparse categorical crossentropy

--- unet/model.py
+++ unet/model.py
@@ -54,7 +54,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
 
     model = Model(inputs = inputs, outputs = conv10)
 
-    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
     
     #model.summary()
 
