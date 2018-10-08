a95fb1d7519da08ca0f171180859b22e49c9df69
Scott White <whitews@gmail.com>
2018-10-08 19:08:29 -0400

    calculate accuracy on test set, save true and predicted labels as CSV

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from utils.data import create_generator_from_stash
 from keras.models import load_model
 from classifier.architecture import weighted_categorical_crossentropy
+import numpy as np
+import pandas as pd
 
 gen = create_generator_from_stash('model_data/test_numpy', batch_size=200)
-x, _ = next(gen)
+x, y = next(gen)
 
 model = load_model(
     'model_data/model_1.hdf5',
@@ -12,10 +14,12 @@ model = load_model(
 
 pred = model.predict(x)
 
-gen = create_generator_from_stash('model_data/test_numpy', batch_size=400)
-x, _ = next(gen)
+pred_label = pred.argmax(axis=1)
+true_label = y.argmax(axis=1)
 
-pred2 = model.predict(x)
+df = pd.DataFrame(np.vstack((true_label, pred_label)).T, columns=['true', 'predicted'])
+df.to_csv('model_data/results.csv')
 
-# TODO: load the OHE labels and compare the predictions
-
+true_pred = pred_label == true_label
+accuracy = np.sum(true_pred) / true_pred.shape[0]
+print (accuracy)
