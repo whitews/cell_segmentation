11b0aaea10713cd7eded26aa85403adcd193d5d4
Scott White <whitews@gmail.com>
2018-10-08 19:13:31 -0400

    use final trained model for testing

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ gen = create_generator_from_stash('model_data/test_numpy', batch_size=200)
 x, y = next(gen)
 
 model = load_model(
-    'model_data/model_1.hdf5',
+    'model_data/model_2.hdf5',
     custom_objects={'loss':  weighted_categorical_crossentropy((1, 6, 6, 13, 13))}
 )
 
@@ -22,4 +22,4 @@ df.to_csv('model_data/results.csv')
 
 true_pred = pred_label == true_label
 accuracy = np.sum(true_pred) / true_pred.shape[0]
-print (accuracy)
+print(accuracy)
