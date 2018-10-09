bb29d1e8b30c268dec018b74652834540997aca0
Scott White <whitews@gmail.com>
2018-10-08 20:12:50 -0400

    update the test script to save the results with the string labels

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from keras.models import load_model
 from classifier.architecture import weighted_categorical_crossentropy
 import numpy as np
 import pandas as pd
+import json
 
 gen = create_generator_from_stash('model_data/test_numpy', batch_size=200)
 x, y = next(gen)
@@ -14,12 +15,19 @@ model = load_model(
 
 pred = model.predict(x)
 
-pred_label = pred.argmax(axis=1)
-true_label = y.argmax(axis=1)
+pred_code = pred.argmax(axis=1)
+true_code = y.argmax(axis=1)
 
-df = pd.DataFrame(np.vstack((true_label, pred_label)).T, columns=['true', 'predicted'])
+df = pd.DataFrame(np.vstack((true_code, pred_code)).T, columns=['true', 'predicted'])
+
+label_lut = json.load(open('model_data/test_numpy/ohe_meta.json'))
+
+for code, label in label_lut.items():
+    df.loc[df.true == int(code), 'true_label'] = label
+    df.loc[df.predicted == int(code), 'pred_label'] = label
+
 df.to_csv('model_data/results.csv')
 
-true_pred = pred_label == true_label
+true_pred = pred_code == true_code
 accuracy = np.sum(true_pred) / true_pred.shape[0]
 print(accuracy)
