5e9aaa2c8c0273f9545d80c5d825ecbc477b4f56
Scott White <whitews@gmail.com>
2018-10-10 14:43:42 -0400

    compute cross tabulation for test results

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 from utils.data import create_generator_from_stash
 from keras.models import load_model
-from classifier.architecture import weighted_categorical_crossentropy
 import numpy as np
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
 
 gen = create_generator_from_stash('model_data/test_numpy', batch_size=200)
 x, y = next(gen)
 
-model = load_model(
-    'model_data/model_2.hdf5',
-    custom_objects={'loss':  weighted_categorical_crossentropy((1, 6, 6, 13, 13))}
-)
+model = load_model('model_data/model_1_plain.hdf5')
 
 pred = model.predict(x)
 
@@ -26,8 +23,21 @@ for code, label in label_lut.items():
     df.loc[df.true == int(code), 'true_label'] = label
     df.loc[df.predicted == int(code), 'pred_label'] = label
 
-df.to_csv('model_data/results.csv')
+df.to_csv('model_data/results_model1.csv')
 
 true_pred = pred_code == true_code
 accuracy = np.sum(true_pred) / true_pred.shape[0]
 print(accuracy)
+
+results = pd.crosstab(true_code, pred_code)
+
+checkindex = np.where(np.not_equal(true_code, pred_code))
+
+for i in checkindex[0]:
+    yhat = np.argmax(pred[i], axis=-1)
+    yhat_lab = label_lut[str(yhat)]
+    truth = np.argmax(y[i], axis=-1)
+    truth_lab = label_lut[str(truth)]
+    plt.title('Truth: {}, Prediction {}'.format(truth_lab, yhat_lab))
+    plt.imshow(x[i, :, :, :])
+    plt.savefig('classifier_missed' + str(i) + '.png')
