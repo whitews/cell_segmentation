a08628c02571830bb3df8b99c301ecbceb48d169
whitews <whitews@gmail.com>
2018-10-17 16:01:10 -0400

    update data generation to include extra feature metrics and make it easier to switch between masked/balanced options

--- data_generator.py
+++ data_generator.py
@@ -1,17 +1,48 @@
 from utils.data import get_imageset_in_memory, clean_and_stash_numpys
 import os
 
-cwd = os.getcwd()
-train_dir = os.path.join(cwd, 'model_data/train_numpy')
-test_dir = os.path.join(cwd, 'model_data/test_numpy')
-test_img_name = '2015-04-029_20X_C57Bl6_E16.5_LMM.14.24.4.46_SOX9_SFTPC_ACTA2_001.tif'
+mask_opt = 'masked'
+color_opt = 'full'
+balance_opt = 'unbalanced'
 
-if not os.path.exists(train_dir):
-    os.makedirs(train_dir)
-if not os.path.exists(test_dir):
-    os.makedirs(test_dir)
+cwd = os.getcwd()
+parent_dir = 'model_data'
+opt_str = "_".join([mask_opt, color_opt, balance_opt])
+
+train_dir = "_".join(['train', 'numpy', opt_str])
+train_path = os.path.join(cwd, parent_dir, train_dir)
+test_dir = "_".join(['test', 'numpy', opt_str])
+test_path = os.path.join(cwd, parent_dir, test_dir)
 
 image_set_dir = os.path.join(cwd, 'data/image_set_73')
-x, y, test_x, test_y = get_imageset_in_memory(image_set_dir, test_img_name)
-clean_and_stash_numpys(x, y, train_dir)
-clean_and_stash_numpys(test_x, test_y, test_dir)
+test_img_name = '2015-04-029_20X_C57Bl6_E16.5_LMM.14.24.4.46_SOX9_SFTPC_ACTA2_001.tif'
+
+if not os.path.exists(train_path):
+    os.makedirs(train_path)
+if not os.path.exists(test_path):
+    os.makedirs(test_path)
+
+if mask_opt == 'masked':
+    is_masked = True
+else:
+    is_masked = False
+
+if balance_opt == 'balanced':
+    is_balanced = True
+else:
+    is_balanced = False
+
+if color_opt == 'pseudo':
+    is_pseudo = True
+else:
+    is_pseudo = False
+
+x, extra_x, y, test_x, test_extra_x, test_y = get_imageset_in_memory(
+    image_set_dir,
+    test_img_name,
+    balance_train=is_balanced,
+    masked=is_masked,
+    pseudo_color=is_pseudo
+)
+clean_and_stash_numpys(x, extra_x, y, train_path)
+clean_and_stash_numpys(test_x, test_extra_x, test_y, test_path)
--- utils/data.py
+++ utils/data.py
@@ -8,9 +8,7 @@ except ImportError:
     import cv2
 from operator import itemgetter
 import numpy as np
-from random import randint
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from keras.preprocessing.image import ImageDataGenerator
 import skimage.transform as trans
 from collections import Counter
 import itertools
@@ -29,39 +27,166 @@ rgb_color_lut = {
     'black': (0, 0, 0)
 }
 
+rgb2_color_lut = {
+    'dark_blue': (0, 0, 160),
+    'white_blue': (160, 160, 255)
+}
 
-def create_generator_from_stash(stash_dir, batch_size=12, demo_gen=False):
-    x = np.load(os.path.join(stash_dir, 'images.npy'))
-    y = np.load(os.path.join(stash_dir, 'ohelabels.npy'))
-    image_datagen = ImageDataGenerator(
-        horizontal_flip=True,
-        vertical_flip=True
-    )
-
-    if demo_gen:
-        index = randint(0, x.shape[1])
-        x = np.expand_dims(x[index, :], axis=0)
-        y = np.expand_dims(y[index, :], axis=0)
-    image_datagen.fit(x)
-    return image_datagen.flow(x, y, batch_size=batch_size)
+include_features = [
+    'region_area',
+    'region_saturation_mean',
+    'region_saturation_variance',
+    # 'region_value_mean',
+    # 'region_value_variance',
+    'percent (black)',
+    'percent (white)',
+    'percent (gray)',
+    'percent (red)',
+    'percent (green)',
+    'percent (blue)',
+    'percent (violet)',
+    'percent (yellow)',
+    'percent (cyan)',
+    # 'percent (white_blue)',
+    'largest_contour_area (black)',
+    'largest_contour_area (white)',
+    'largest_contour_area (gray)',
+    'largest_contour_area (red)',
+    'largest_contour_area (green)',
+    'largest_contour_area (blue)',
+    'largest_contour_area (violet)',
+    'largest_contour_area (yellow)',
+    'largest_contour_area (cyan)',
+    # 'largest_contour_area (white_blue)',
+    'largest_contour_eccentricity (black)',
+    'largest_contour_eccentricity (white)',
+    'largest_contour_eccentricity (gray)',
+    'largest_contour_eccentricity (red)',
+    'largest_contour_eccentricity (green)',
+    'largest_contour_eccentricity (blue)',
+    'largest_contour_eccentricity (violet)',
+    'largest_contour_eccentricity (yellow)',
+    'largest_contour_eccentricity (cyan)',
+    # 'largest_contour_eccentricity (white_blue)',
+    # 'largest_contour_circularity (black)',
+    # 'largest_contour_circularity (white)',
+    # 'largest_contour_circularity (gray)',
+    # 'largest_contour_circularity (red)',
+    # 'largest_contour_circularity (green)',
+    # 'largest_contour_circularity (blue)',
+    # 'largest_contour_circularity (violet)',
+    # 'largest_contour_circularity (yellow)',
+    # 'largest_contour_circularity (cyan)',
+    # 'largest_contour_circularity (white_blue)',
+    'largest_contour_convexity (black)',
+    'largest_contour_convexity (white)',
+    'largest_contour_convexity (gray)',
+    'largest_contour_convexity (red)',
+    'largest_contour_convexity (green)',
+    'largest_contour_convexity (blue)',
+    'largest_contour_convexity (violet)',
+    'largest_contour_convexity (yellow)',
+    'largest_contour_convexity (cyan)',
+    'largest_contour_convexity (white_blue)',
+    'distance_mean (black)',
+    'distance_mean (white)',
+    'distance_mean (gray)',
+    'distance_mean (red)',
+    'distance_mean (green)',
+    'distance_mean (blue)',
+    'distance_mean (violet)',
+    'distance_mean (yellow)',
+    'distance_mean (cyan)',
+    'distance_mean (white_blue)',
+    'distance_variance (black)',
+    'distance_variance (white)',
+    'distance_variance (gray)',
+    'distance_variance (red)',
+    'distance_variance (green)',
+    'distance_variance (blue)',
+    'distance_variance (violet)',
+    'distance_variance (yellow)',
+    'distance_variance (cyan)',
+    'distance_variance (white_blue)',
+    # 'perimeter_mean (black)',
+    # 'perimeter_mean (white)',
+    # 'perimeter_mean (gray)',
+    # 'perimeter_mean (red)',
+    # 'perimeter_mean (green)',
+    # 'perimeter_mean (blue)',
+    # 'perimeter_mean (violet)',
+    # 'perimeter_mean (yellow)',
+    # 'perimeter_mean (cyan)',
+    # 'perimeter_mean (white_blue)',
+    # 'perimeter_variance (black)',
+    # 'perimeter_variance (white)',
+    # 'perimeter_variance (gray)',
+    # 'perimeter_variance (red)',
+    # 'perimeter_variance (green)',
+    # 'perimeter_variance (blue)',
+    # 'perimeter_variance (violet)',
+    # 'perimeter_variance (yellow)',
+    # 'perimeter_variance (cyan)',
+    # 'perimeter_variance (white_blue)',
+    # 'area_mean (black)',
+    # 'area_mean (white)',
+    # 'area_mean (gray)',
+    # 'area_mean (red)',
+    # 'area_mean (green)',
+    # 'area_mean (blue)',
+    # 'area_mean (violet)',
+    # 'area_mean (yellow)',
+    # 'area_mean (cyan)',
+    # 'area_mean (white_blue)',
+    # 'area_variance (black)',
+    # 'area_variance (white)',
+    # 'area_variance (gray)',
+    # 'area_variance (red)',
+    # 'area_variance (green)',
+    # 'area_variance (blue)',
+    # 'area_variance (violet)',
+    # 'area_variance (yellow)',
+    # 'area_variance (cyan)',
+    # 'area_variance (white_blue)',
+    # 'contour_count (black)',
+    # 'contour_count (white)',
+    # 'contour_count (gray)',
+    # 'contour_count (red)',
+    # 'contour_count (green)',
+    # 'contour_count (blue)',
+    # 'contour_count (violet)',
+    # 'contour_count (yellow)',
+    # 'contour_count (cyan)',
+    # 'contour_count (white_blue)',
+]
 
 
 def clean_and_stash_numpys(
         imglist,
+        extra_data,
         strlist,
         numpy_save_dir,
-        imgshape=(299, 299, 3)
+        imgshape=(99, 99, 3)
 ):
     """
     Take a list of numpy arrays and a list of text and return one generator
     """
-    new_mask = np.zeros((len(imglist),) + imgshape)
+    new_mask = np.zeros((len(imglist),) + imgshape, dtype=np.uint8)
     for i, si in enumerate(imglist):
-        img = trans.resize(si, imgshape)
-        new_mask[i, :, :, :] = img
+        max_dim = max(si.shape[:2])
+        si_square = np.zeros((max_dim, max_dim, si.shape[2]), dtype=np.uint8)
+        si_square[:si.shape[0], :si.shape[1], :] = si
+
+        img = trans.resize(
+            si_square,
+            imgshape,
+            mode='constant',
+            anti_aliasing=False,
+            preserve_range=True)
+        new_mask[i, :, :, :] = img.astype(np.uint8)
     le = LabelEncoder()
     strlist_le = le.fit_transform(strlist)
-    ohe = OneHotEncoder(sparse=False)
+    ohe = OneHotEncoder(sparse=False, categories='auto')
     strlist_ohe = ohe.fit_transform(strlist_le.reshape(-1, 1))
     ohe_lut = {}
     for cat in ohe.categories_[0].astype(np.int):
@@ -72,6 +197,7 @@ def clean_and_stash_numpys(
     fh.close()
 
     np.save(os.path.join(numpy_save_dir, 'images.npy'), new_mask)
+    np.save(os.path.join(numpy_save_dir, 'extra.npy'), extra_data)
     np.save(os.path.join(numpy_save_dir, 'ohelabels.npy'), strlist_ohe)
     print('Numpys stashed!')
 
@@ -80,7 +206,6 @@ def get_imageset_in_memory(
         image_set_directory,
         test_image_name,
         balance_train=True,
-        balance_test=False,
         masked=False,
         pseudo_color=False
 ):
@@ -92,8 +217,10 @@ def get_imageset_in_memory(
     test_metadata = image_set_metadata.pop(test_image_name)
 
     images_train = []
+    extra_train = []
     labels_train = []
     images_test = []
+    extra_test = []
     labels_test = []
 
     for key, value in image_set_metadata.items():
@@ -102,8 +229,16 @@ def get_imageset_in_memory(
             hsv_img,
             cv2.COLOR_HSV2RGB
         )
+
         for region in value['regions']:
             labels_train.append(region['label'])
+
+            features = lm_utils.generate_features(hsv_img, region['points'])
+
+            extra_train.append(
+                [features[x] for x in include_features]
+            )
+
             if masked:
                 tmp_img = extract_contour_bounding_box_masked(
                     rgb_img,
@@ -124,6 +259,13 @@ def get_imageset_in_memory(
     )
     for region in test_metadata['regions']:
         labels_test.append(region['label'])
+
+        features = lm_utils.generate_features(hsv_img, region['points'])
+
+        extra_train.append(
+            [features[x] for x in include_features]
+        )
+
         if masked:
             tmp_img = extract_contour_bounding_box_masked(
                 rgb_img,
@@ -137,19 +279,40 @@ def get_imageset_in_memory(
 
         images_test.append(tmp_img)
 
+    extra_train = np.array(extra_train)
+    max_area = extra_train[:, 0].max()
+
+    extra_train[:, 0] = extra_train[:, 0] / max_area
+    extra_test = np.array(extra_test)
+    extra_test[:, 0] = extra_test[:, 0] / max_area
+
     if balance_train:
-        images_train, labels_train = make_balanced(images_train, labels_train)
-    if balance_test:
-        images_test, labels_test = make_balanced(images_test, labels_test)
+        images_train, extra_train, labels_train = make_balanced(
+            images_train,
+            extra_train,
+            labels_train
+        )
+
     assert len(images_train) == len(labels_train)
-    return images_train, labels_train, images_test, labels_test
+
+    processed_data = (
+        images_train,
+        extra_train,
+        labels_train,
+        images_test,
+        extra_test,
+        labels_test
+    )
+
+    return processed_data
 
 
-def make_balanced(xarray, ylist):
+def make_balanced(xarray, extra_array, ylist):
     counts = Counter(ylist)
     bringup = np.max(list(counts.values()))
     bringupdict = {key: bringup//value for key, value in counts.items()}
     newx = []
+    new_extra = []
     newy = []
     for x in set(ylist):
         indices = [i for i, val in enumerate(ylist) if val == x]
@@ -157,8 +320,9 @@ def make_balanced(xarray, ylist):
         newindicesflat = [item for sublist in newindices for item in sublist]
         for index in newindicesflat:
             newx.append(xarray[index])
+            new_extra.append(extra_array[index])
             newy.append(ylist[index])
-    return newx, newy
+    return newx, new_extra, newy
 
 
 def get_training_data_for_image_set(image_set_dir):
@@ -239,6 +403,11 @@ def extract_contour_bounding_box_masked(image, contour):
 def generate_pseudo_color_image(rgb_image):
     pseudo_color_img = np.zeros(rgb_image.shape, dtype=np.uint8)
     for color, value in rgb_color_lut.items():
+        color_mask = lm_utils.create_mask(cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV), [color])
+
+        pseudo_color_img[color_mask > 0] = value
+
+    for color, value in rgb2_color_lut.items():
         color_mask = lm_utils.create_mask(cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV), [color])
 
         pseudo_color_img[color_mask > 0] = value
